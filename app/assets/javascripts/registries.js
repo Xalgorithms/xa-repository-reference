43afae7b01613bf0400cd42ced1fd28e1bdef5b3
Don Kelly <karfai@gmail.com>
2016-06-13 22:44:40 -0400

    Add remove to registries

--- app/assets/javascripts/registries.js
+++ app/assets/javascripts/registries.js
@@ -1,10 +1,26 @@
-$(document).on('ready page:load', function () {
-  attach_action_delete();
+function register_destroy() {
+  $('.destroy').on('ajax:success', function (e, o) {
+    vm.registries.remove(function (it) {
+      return it._id.$oid === o.id;
+    });
+  });
+}
 
+$(document).on('ready page:load', function () {
   $('#new_registry').on('ajax:success', function (e, o) {
     $('#modal-add-registry').modal('toggle');
     vm.registries.push(o);
+    register_destroy();
   });
 
+  vm.any_registries = ko.computed(function () {
+    return vm.registries().length > 0;
+  });
+
+  vm.format_registry_url = function (o) {
+    return Routes.api_v1_registry_path({ id: o._id.$oid });
+  };
+
   ko.applyBindings(vm, $('#binding-point')[0]);
+  register_destroy();
 });
