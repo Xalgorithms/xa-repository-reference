6d1027f1d5ad04fb947b41bcc660a03bace53fdf
Don Kelly <karfai@gmail.com>
2016-06-20 22:20:35 -0400

    Factor common JS

--- app/assets/javascripts/rules.js
+++ app/assets/javascripts/rules.js
@@ -1,26 +1,12 @@
-function register_destroy() {
-  $('.destroy').on('ajax:success', function (e, o) {
-    vm.rules.remove(function (it) {
-      return it.id === o.id;
-    });
-  });
-}
 init_on_page('rules', function () {
-  $('#new_rule').on('ajax:success', function (e, o) {
-    $('#modal-add-rule').modal('toggle');
-    debugger;
-    vm.rules.push(o);
-    register_destroy();
+  manage_collection(vm, {
+    collection: 'rules',
+    modals: {
+      add: '#modal-add-rule'
+    },
+    actions: {
+      add: '#new_rule'
+    },
+    path: Routes.api_v1_rule_path
   });
-
-  vm.any_rules = ko.computed(function () {
-    return vm.rules().length > 0;
-  });
-
-  vm.format_rule_url = function (o) {
-    return Routes.api_v1_rule_path({ id: o.id });
-  };
-
-  ko.applyBindings(vm, $('#binding-point')[0]);
-  register_destroy();
 });
