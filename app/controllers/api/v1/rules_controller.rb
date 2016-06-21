c3f9dd847478ab261132feeef3311b813931f379
Don Kelly <karfai@gmail.com>
2016-06-20 21:56:00 -0400

    UI for rules

--- app/controllers/api/v1/rules_controller.rb
+++ app/controllers/api/v1/rules_controller.rb
@@ -1,10 +1,23 @@
 module Api
   module V1
     class RulesController < ActionController::Base
+      def create
+        @rule = Rule.create(rule_args)
+
+        render(json: { id: @rule.id.to_s, name: @rule.name } )
+      end
+      
       def destroy
         @rule = Rule.find(params[:id])
         @rule.destroy
+
         render(json: { id: params[:id] })
+      end
+
+      private
+
+      def rule_args
+        params.require(:rule).permit(:name, :src)
       end
     end
   end
--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,5 +1,5 @@
 class RulesController < ApplicationController
   def index
-    @rules = Rule.all
+    @rules = Rule.all.map { |r| { id: r._id.to_s, name: r.name } }
   end
 end
