11473ec9beb5128631abfe12c00b8b4de4e7b910
Don Kelly <karfai@gmail.com>
2016-07-15 00:18:15 -0400

    Fix page load event

--- app/assets/javascripts/globals.js
+++ app/assets/javascripts/globals.js
@@ -5,7 +5,7 @@ function only_on_page(name, fn) {
 }
 
 function init_on_page(name, fn) {
-  $(document).on('ready page:load', function () {
+  $(document).on('turbolinks:load', function () {
     only_on_page(name, function() {
       fn();
     });
