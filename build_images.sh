7c9c55b50f08173e25a0da618ad97dfa5a4b1563
Don Kelly <karfai@gmail.com>
2016-10-02 14:15:22 -0400

    travis: only build production image on releases branch

--- build_images.sh
+++ build_images.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-docker build -t karfai42/xa-repository-reference:production -f Dockerfile.production .
+if [ "$TRAVIS_BRANCH" == "releases" ]; then
+    docker build -t karfai42/xa-repository-reference:production -f Dockerfile.production .
+fi
+
 if [ ! -z "$TRAVIS_TAG" ]; then
     docker build -t karfai42/xa-repository-reference:$TRAVIS_TAG -t karfai42/xa-repository-reference:latest -f Dockerfile.production .
 fi
--- publish_images.sh
+++ publish_images.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push karfai42/xa-repository-reference:production
+
+if [ "$TRAVIS_BRANCH" == "releases" ]; then
+    docker push karfai42/xa-repository-reference:production
+fi
+
 if [ ! -z "$TRAVIS_TAG" ]; then
     docker push karfai42/xa-repository-reference:$TRAVIS_TAG
     docker push karfai42/xa-repository-reference:latest
