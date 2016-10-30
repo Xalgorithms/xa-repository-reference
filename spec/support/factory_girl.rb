3ea983d92b906186286e1f2acd18e827e8b8d25d
Don Kelly <karfai@gmail.com>
2016-10-30 14:16:08 -0400

    RSpec configuration

---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,12 @@
+require 'factory_girl'
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    begin
+      FactoryGirl.lint
+    ensure
+    end
+  end
+end
