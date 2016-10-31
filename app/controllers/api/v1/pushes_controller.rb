998cd5b8224cf3377ccbca4546ad1399fd03d03e
Don Kelly <karfai@gmail.com>
2016-10-30 20:36:48 -0400

    Define a basic controller to receive webhook events from github
    Eventually, we will support webhook POSTs from github. This is a place
    holder until we can first clone a basic, generic, git repository.xsxc

---
+++ app/controllers/api/v1/pushes_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    class PushesController < ActionController::Base
+    end
+  end
+end
---
+++ spec/controllers/api/v1/pushes_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe Api::V1::PushesController, type: :controller do
+  include Randomness
+  include ResponseJson
+end
