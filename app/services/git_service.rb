d211e4fac61810f093bf56c50c2e51598ab6d047
Don Kelly <karfai@gmail.com>
2016-11-02 02:14:24 -0400

    finish git service

--- app/services/git_service.rb
+++ app/services/git_service.rb
@@ -21,23 +21,22 @@ class GitService
 
   def self.scan_all(repo)
     br = repo.branches['master']
+    ver = br.target.oid[0..7]
     to_scan = {}
-    br.target.tree.each do |o|
-      if o[:type] == :tree
-        Rails.logger.debug("scanning tree (name=#{o[:name]}; oid=#{o[:oid]})")
-        files = repo.lookup(o[:oid]).inject({}) do |fs, o|
-          n = o[:name]
-          ext = File.extname(n)
-          rt = ext[1..-1].to_sym
-
-          Rails.logger.debug("scanning file (name=#{o[:name]}; rt=#{rt}; oid=#{o[:oid]})")
-          blob = repo.lookup(o[:oid])
-          vals = { name: File.basename(n, ext), oid: o[:oid] }
-          Rule::TYPES.include?(rt) ? fs.merge(rt => fs.fetch(rt, []).push(vals)) : fs
-        end
-
-        to_scan = to_scan.merge(o[:name] => files)
+    br.target.tree.each_tree do |o|
+      Rails.logger.debug("scanning tree (name=#{o[:name]}; oid=#{o[:oid]}; ver=#{ver})")
+      files = repo.lookup(o[:oid]).inject([]) do |fs, o|
+        n = o[:name]
+        ext = File.extname(n)
+        rt = ext[1..-1].to_sym
+        
+        Rails.logger.debug("scanning file (name=#{o[:name]}; rt=#{rt}; oid=#{o[:oid]})")
+        blob = repo.lookup(o[:oid])
+        vals = { name: File.basename(n, ext), type: rt, oid: o[:oid], ver: ver }
+        Rule::TYPES.include?(rt) ? fs.push(vals) : fs
       end
+      
+      to_scan = to_scan.merge(o[:name] => files)
     end
 
     to_scan
@@ -45,14 +44,31 @@ class GitService
 
   def self.add_all(repo, all)
     Rails.logger.info('adding new objects')
-    all.each do |ns_name, types|
+    all.each do |ns_name, entries|
       nsm = Namespace.find_or_create_by(name: ns_name)
-      types.each do |rt, vals|
-        vals.each do |v|
-          blob = repo.lookup(v[:oid])
-          RuleService.create(nsm, v[:name], blob.content, rt)
-        end
+
+      ap entries
+      vers = entries.inject({}) do |o, e|
+        o.merge(e[:name] => e[:ver])
+      end
+
+      ap vers
+      entries.each do |o|
+        blob = repo.lookup(o[:oid])
+        content = :xalgo == o[:type] ? replace_versions(blob.content, vers) : blob.content
+        RuleService.create(nsm, o[:name], content, o[:type], o[:ver])
       end if nsm
     end
+  end
+
+  def self.replace_versions(content, vers)
+    content.split(/[\r]?\n/).map do |ln|
+      m = /\{((?:\w+))_version\}/.match(ln)
+      if m
+        ln = ln.gsub("{#{m[1]}_version}", vers[m[1]].to_s)
+      end
+
+      ln
+    end.join("\n")
   end
 end
