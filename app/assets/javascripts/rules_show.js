dd7a3fdc66df653a34c8bb95e5a1d83de20f18d3
Don Kelly <karfai@gmail.com>
2017-03-14 02:05:20 -0400

    Add page controls for creating and running trials

--- app/assets/javascripts/rules_show.js
+++ app/assets/javascripts/rules_show.js
@@ -4,6 +4,7 @@
 
     var page_vm = _.extend({}, rule, {
       active_version: ko.observable(_.head(rule.versions)),
+      active_trial: ko.observable(_.last(rule.trials)),
       versions_loaded: ko.observable(false),
       is_xalgo: ko.observable('xalgo' === rule.type),
       is_table: ko.observable('table' === rule.type),
@@ -13,6 +14,23 @@
     page_vm.any_trials = ko.computed(function () {
       return _.size(page_vm.trials()) > 0;
     });
+
+    page_vm.start_trial = function () {
+      var ev = {
+	events_trial_add: { rule_id : rule.id }
+      };
+      
+      send_event(ev, function (eo) {
+	$.getJSON(eo.url, function (o) {
+	  page_vm.trials.push(o);
+	  page_vm.active_trial(o);
+	});
+      });
+    };
+
+    page_vm.run_trial = function () {
+      console.log('TBD');
+    };
     
     // get all the versions
     _.each(rule.versions, function (ver) {
