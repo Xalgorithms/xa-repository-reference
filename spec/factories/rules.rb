9fd543af4bbcbc34776482d620f9b0e35318fa2e
Don Kelly <karfai@gmail.com>
2017-03-14 00:38:39 -0400

    Add trials to rule serialization

--- spec/factories/rules.rb
+++ spec/factories/rules.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :rule do
     name { Faker::Hipster.word }
     namespace { create(:namespace) }
+    rule_type { 'xalgo' }
   end
 end
---
+++ spec/serializers/rule_serializer_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe RuleSerializer do
+  after(:each) do
+    Rule.destroy_all
+    Trial.destroy_all
+    Version.destroy_all
+  end
+
+  def make_expected(rm)
+    {
+      id: rm.public_id,
+      name: rm.name,
+      type: rm.rule_type,
+      versions: rm.versions.map { |vm| vm.code },
+      namespace: { name: rm.namespace.name },
+      trials: rm.trials.map { |tm| { id: tm.public_id, label: tm.label } },
+    }
+  end
+
+  it 'should serialize' do
+    rand_array_of_models(:rule).each do |rm|
+      rand_array_of_models(:version, rule: rm)
+      rand_array_of_models(:trial, rule: rm)
+      
+      expect(RuleSerializer.as_json(rm)).to eql(make_expected(rm))
+    end
+  end
+
+  it 'should serialize many' do
+    rms = rand_array_of_models(:rule)
+    rms.each { |rm| rand_array_of_models(:version, rule: rm) }
+    expected = rms.map(&method(:make_expected))
+    expect(RuleSerializer.many(rms)).to eql(expected)
+  end
+end
