720b3a447a66c7ceaf0deb592bfe38f9d1502221
Don Kelly <karfai@gmail.com>
2017-04-10 00:21:46 -0400

    Adds namespace/add modal

--- app/assets/javascripts/namespaces_index.js
+++ app/assets/javascripts/namespaces_index.js
@@ -1,6 +1,12 @@
 (function () {
   function init() {
     var page_vm = {
+      modals: {
+	add: {
+	  active: ko.observable(false),
+	  name: ko.observable()	  
+	}
+      }      
     };
 
     function make_namespace_vm(o) {
@@ -22,6 +28,31 @@
     }
 
     page_vm.namespaces = ko.observableArray(_.map(namespaces, make_namespace_vm));
+
+    page_vm.add = function () {
+      page_vm.modals.add.active(true);
+    };
+
+    page_vm.modals.add.send = function (vm) {
+      var ev = {
+	events_namespace_add: {
+	  name: vm.name()
+        }
+      };      
+
+      send_event(ev, function (eo) {
+      	$.getJSON(eo.url, function (nso) {
+	  page_vm.namespaces.push(make_namespace_vm(nso));
+      	  vm.active(false);
+      	});
+      });
+      
+      vm.active(false);
+    };
+
+    page_vm.modals.add.deactivate = function (vm) {
+      vm.active(false);
+    };
 
     ko.applyBindings(page_vm, document.getElementById('page'));
   }
